e5b29f69c302c7b4969bfe56ebbc1b90c9e0774b
Ella-1 <batazoaka3@gmail.com>
2023-07-27 10:56:22 +0100

    frontend interview project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
+import Food from './components/Food';
+import Category from './components/category';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className=''></div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category /> 
+
+    </>
+  )
+}
+
+export default App
